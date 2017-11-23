b3d457c72bf1264d482fe7d0c4a82e4e92cb2912
YamakaziMouha <mouha.chelali@gmail.com>
2017-11-23 19:09:34 +0100

    rien de special

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -5,12 +5,12 @@ from PySide import QtGui
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from scripts import ColorDeconvolution as CD
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-
-
+        self.mat=None # cette variable contient l'image charger
         self.initUI()
 
     def initUI(self):
@@ -19,14 +19,14 @@ class MainWindow(QtGui.QMainWindow):
         self.canvas = FigureCanvas(self.figure)
         self.setCentralWidget(self.canvas)
         # ------ Les Boutons conecte avec sa fonction ------------
-        load = QtGui.QAction(QtGui.QIcon(""), "Load Image", self)
+        load = QtGui.QAction(QtGui.QIcon(""), "Load Image", self) # Ce boutton permet de charger une image
         load.triggered.connect(self.browse_on)
-        exitAction = QtGui.QAction(QtGui.QIcon(""), 'Exit', self)
+        exitAction = QtGui.QAction(QtGui.QIcon(""), 'Exit', self) # Ce boutton permet de fermer l'application
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(self.close)
 
         # toolbar
-        self.toolbar = self.addToolBar('Exit')
+        self.toolbar = self.addToolBar("ToolBar")
         self.toolbar.addAction(load)
         self.toolbar.addAction(exitAction)
 
@@ -43,14 +43,12 @@ class MainWindow(QtGui.QMainWindow):
             self.print_img(self.mat)
 
     def print_img(self, matrice):
-        try:
-            if matrice.shape[2] == 3:
-                plt.imshow(matrice)
-        except:
+        if len(matrice.shape) == 3:
+            plt.imshow(matrice)
+        else:
             plt.imshow(matrice, cmap=cm.Greys_r)
         plt.axis('off')
         self.canvas.draw()
-
 
 def main():
     app = QtGui.QApplication(sys.argv)
