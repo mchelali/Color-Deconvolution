e0efbff8a60c84c5e71bf8e7b022331bd78435e7
ro0t34 <mouha.chelali@gmail.com>
2018-01-26 00:21:33 +0100

    Creation d'une interface pour PCA et ICA

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,54 +1,273 @@
 #coding: utf-8
 
 import sys
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+import cv2
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from scripts import ColorDeconvolution as CD
+from scripts import pca as Stain
+import ICAWindow as methodes
+import numpy as np
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.mat=None # cette variable contient l'image charger
+
+        self.image = None # Input Image
+
         self.initUI()
 
     def initUI(self):
         # ---- La figure ou se place les images -----------
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.setCentralWidget(self.canvas)
-        # ------ Les Boutons conecte avec sa fonction ------------
-        load = QtGui.QAction(QtGui.QIcon(""), "Load Image", self) # Ce boutton permet de charger une image
-        load.triggered.connect(self.browse_on)
-        exitAction = QtGui.QAction(QtGui.QIcon(""), 'Exit', self) # Ce boutton permet de fermer l'application
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.triggered.connect(self.close)
+        self.printer = QtGui.QPrinter()
+        self.scaleFactor = 0.0
 
-        # toolbar
-        self.toolbar = self.addToolBar("ToolBar")
-        self.toolbar.addAction(load)
-        self.toolbar.addAction(exitAction)
+        self.imageLabel = QtGui.QLabel()
+        self.imageLabel.setBackgroundRole(QtGui.QPalette.Base)
+        self.imageLabel.setSizePolicy(QtGui.QSizePolicy.Ignored,
+                                      QtGui.QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.setCentralWidget(self.scrollArea)
+        self.createActions()
+        self.createMenus()
+
+
+
+        self.resize(500, 400)
 
         self.setWindowTitle('Stain separation')
 
         self.show()
 
     def browse_on(self):
-        filename = str(QtGui.QFileDialog.getOpenFileName(None, 'Open file')[0])
-        print filename
-        if filename != '':
-            # self.print_img(filename)
-            self.mat = plt.imread(filename)
-            self.print_img(self.mat)
+        fileName, _ = QtGui.QFileDialog.getOpenFileName(self, "Open File",
+                                                        QtCore.QDir.currentPath())
+        if fileName:
+            """
+            Lire l'image si le chemin n'est pas vide
 
-    def print_img(self, matrice):
-        if len(matrice.shape) == 3:
-            plt.imshow(matrice)
-        else:
-            plt.imshow(matrice, cmap=cm.Greys_r)
-        plt.axis('off')
-        self.canvas.draw()
+            Aussi ici je fait la gestion du typr d'image pour pouvoir le convertir du type numpy.ndarray a QImage
+            """
+            self.image = plt.imread(fileName)
+            self.print_img(self.image)
+            self.l, self.c, self.d = self.image.shape
+            self.stain = Stain.ICA_StainSeparation(self.image.astype(np.float))
+
+
+
+    def print_img(self, image):
+        imageQt = None
+        if len(image.shape) == 2:
+            imageQt = QtGui.QImage(image.data, image.shape[1], image.shape[0], QtGui.QImage.Format_Indexed8)
+        elif len(image.shape) == 3:
+            #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            if image.shape[2] == 3:
+                print "shape = 3"
+                imageQt = QtGui.QImage(image.data, image.shape[1], image.shape[0], image.shape[2] * image.shape[1],
+                                       QtGui.QImage.Format_RGB888)
+
+                """elif image.shape[2] == 4:
+                    print "shape = 4"
+                    print image[0,0]
+                    image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
+                    imageQt = QtGui.QImage(image.data, image.shape[1], image.shape[0], image.shape[2] * image.shape[1],
+                                           QtGui.QImage.Format_RGB888)"""
+            else:
+                imageQt = QtGui.QImage()
+        if imageQt.isNull():
+            QtGui.QMessageBox.information(self, "Image Viewer", "Cannot load .")
+            return
+        self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(imageQt))
+        self.scaleFactor = 1.0
+
+        self.printAct.setEnabled(True)
+        self.fitToWindowAct.setEnabled(True)
+        self.updateActions()
+
+        if not self.fitToWindowAct.isChecked():
+            self.imageLabel.adjustSize()
+
+    def print_(self):
+        dialog = QtGui.QPrintDialog(self.printer, self)
+        if dialog.exec_():
+            painter = QtGui.QPainter(self.printer)
+            rect = painter.viewport()
+            size = self.imageLabel.pixmap().size()
+            size.scale(rect.size(), QtCore.Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.imageLabel.pixmap().rect())
+            painter.drawPixmap(0, 0, self.imageLabel.pixmap())
+
+    def zoomIn(self):
+        self.scaleImage(1.25)
+
+    def zoomOut(self):
+        self.scaleImage(0.8)
+
+    def normalSize(self):
+        self.imageLabel.adjustSize()
+        self.scaleFactor = 1.0
+
+    def fitToWindow(self):
+        fitToWindow = self.fitToWindowAct.isChecked()
+        self.scrollArea.setWidgetResizable(fitToWindow)
+        if not fitToWindow:
+            self.normalSize()
+
+        self.updateActions()
+
+    def about(self):
+        QtGui.QMessageBox.about(self, "Stain separation",
+                                """
+                                Cette application a ete cree dans le context du projet de fin
+                                d'etude de master 2 en Image et Pluremedia
+                                """)
+
+    def createActions(self):
+        """
+        Creation des actions qu'on peut faire ex: pour ouverir un fichier, zoom ....
+        """
+        self.openAct = QtGui.QAction("&Open...", self, shortcut="Ctrl+O",
+                                     triggered=self.browse_on)
+
+        self.printAct = QtGui.QAction("&Print...", self, shortcut="Ctrl+P",
+                                      enabled=False, triggered=self.print_)
+
+        self.exitAct = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q",
+                                     triggered=self.close)
+
+        self.zoomInAct = QtGui.QAction("Zoom &In (25%)", self,
+                                       shortcut="Ctrl++", enabled=False, triggered=self.zoomIn)
+
+        self.zoomOutAct = QtGui.QAction("Zoom &Out (25%)", self,
+                                        shortcut="Ctrl+-", enabled=False, triggered=self.zoomOut)
+
+        self.normalSizeAct = QtGui.QAction("&Normal Size", self,
+                                           shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
+
+        self.fitToWindowAct = QtGui.QAction("&Fit to Window", self,
+                                            enabled=False, checkable=True, shortcut="Ctrl+F",
+                                            triggered=self.fitToWindow)
+
+        self.aboutAct = QtGui.QAction("&About", self, triggered=self.about)
+
+        self.aboutQtAct = QtGui.QAction("About &Qt", self,
+                                        triggered=QtGui.qApp.aboutQt)
+        self.pca = QtGui.QAction("pca methode", self,
+                                        triggered=self.pca_methode)
+        self.ica = QtGui.QAction("ica method", self,
+                                        triggered=self.ica_methode)
+
+        self.binPca = QtGui.QAction("binarize pca", self,
+                                        triggered=self.pca_binarization)
+        self.binICA = QtGui.QAction("binarize ica", self,
+                                        triggered=self.ica_binarization)
+
+    def createMenus(self):
+        """
+        Organisation des actions dans des menus
+        """
+        self.fileMenu = QtGui.QMenu("&File", self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.printAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.meth = QtGui.QMenu("&Methode", self)
+        self.meth.addAction(self.pca)
+        self.meth.addAction(self.binPca)
+        self.meth.addSeparator()
+        self.meth.addAction(self.ica)
+        self.meth.addAction(self.binICA)
+
+        self.viewMenu = QtGui.QMenu("&View", self)
+        self.viewMenu.addAction(self.zoomInAct)
+        self.viewMenu.addAction(self.zoomOutAct)
+        self.viewMenu.addAction(self.normalSizeAct)
+        self.viewMenu.addSeparator()
+        self.viewMenu.addAction(self.fitToWindowAct)
+
+        self.helpMenu = QtGui.QMenu("&Help", self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.meth)
+        self.menuBar().addMenu(self.viewMenu)
+        self.menuBar().addMenu(self.helpMenu)
+        print "binarization ica"
+    def updateActions(self):
+        self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def scaleImage(self, factor):
+        self.scaleFactor *= factor
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
+
+        self.zoomInAct.setEnabled(self.scaleFactor < 3.0)
+        self.zoomOutAct.setEnabled(self.scaleFactor > 0.333)
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                               + ((factor - 1) * scrollBar.pageStep() / 2)))
+
+    def pca_methode(self):
+        print "pca work"
+        self.stain.RGB_2_OD()
+        self.stain.lanchePCA()
+        self.img_pca = self.stain.normalisation2(self.stain.pca_).reshape((self.l, self.c, 3))
+        self.pca_view = methodes.ICAWindow()
+        self.pca_view.addImage(self.img_pca)
+        self.pca_view.show_()
+        self.pca_view.show()
+
+    def ica_methode(self):
+        print "ica work"
+        self.stain.lanchICA()
+        self.img_ica = self.stain.normalisation2(self.stain.ica_).reshape((self.l, self.c, 3))
+        self.ica_view = methodes.ICAWindow()
+        self.ica_view.addImage(self.img_ica)
+        self.ica_view.show_()
+        self.ica_view.show()
+
+    def pca_binarization(self):
+        print "binarization pca"
+        pca1 = np.uint8(self.img_pca[:, :, 0])
+        pca2 = np.uint8(self.img_pca[:, :, 1])
+        pca3 = np.uint8(self.img_pca[:, :, 2])
+
+        bin_pca = np.zeros([self.l, self.c, self.d])
+        thres1, bin_pca[:, :, 0] = cv2.threshold(pca1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thres2, bin_pca[:, :, 1] = cv2.threshold(pca2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thres3, bin_pca[:, :, 2] = cv2.threshold(pca3, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        self.pca_view.addImage(bin_pca)
+        self.pca_view.show_()
+        self.pca_view.show()
+
+    def ica_binarization(self):
+        print "binarization ica"
+        ica1 = np.uint8(self.img_ica[:, :, 0])
+        ica2 = np.uint8(self.img_ica[:, :, 1])
+        ica3 = np.uint8(self.img_ica[:, :, 2])
+
+        bin_ica = np.zeros([self.l, self.c, self.d])
+        thres1, bin_ica[:, :, 0] = cv2.threshold(ica1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thres2, bin_ica[:, :, 1] = cv2.threshold(ica2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thres3, bin_ica[:, :, 2] = cv2.threshold(ica3, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        self.ica_view.addImage(bin_ica)
+        self.ica_view.show_()
+        self.ica_view.show()
+
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
--- scripts/pca.py
+++ scripts/pca.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA, FastICA
@@ -20,35 +21,62 @@ class ICA_StainSeparation:
         for i in range(self.size[0]*self.size[1]):
             for j in range(self.size[2]):
                 if self.img[i, j] != 0:
-                    self.od[i, j] = np.log(self.img[i, j])
+                    self.od[i, j] = np.log10(self.img[i, j])
 
     def lanchePCA(self):
         # Application de la PCA
         pca = PCA(n_components=3)
         self.pca_ = pca.fit_transform(self.od)
         self.I_r =  np.dot(self.od.T, self.pca_)
-        print "I_r : ",self.I_r
+
 
     def lanchICA(self):
         # Application de ICA
-        ica = FastICA(n_components=3, algorithm='parallel', whiten=True, fun='logcosh', fun_args=None, max_iter=200,
-                      tol=0.0001, w_init=None, random_state=None)
+        ica = FastICA(n_components=3, algorithm='parallel', whiten=True, fun='logcosh', max_iter=100,
+                      tol=0.0001, w_init=None,random_state=2)
         self.ica_ = ica.fit_transform(self.pca_)
         self.P = np.dot(self.I_r, self.ica_.T)
-        print "P : ", self.P
         self.W = np.dot(self.pca_.T, self.P.T)
-        print "W : ", self.W
 
-    def correctVectorICA(self, stain_number=2):
+
+    def correctVectorICA(self, stain_number=3):
         # Initialisation du vecteur V_min ???
+        v_min = np.zeros(self.ica_.shape)
+        for i in range(self.ica_.shape[2]):
+            v_min = self.ica_[:, i] / np.linalg.norm(self.ica_[:, i])
+        print "v_min   ---->", v_min
 
         #init de la distance  d_i_j
-        d=[]
+        d = []
         for i in range(stain_number):
             d.append(0.1)
+        d_min = 10**(-6)
 
-        #on calcule le cout
-        cost = sum(d)/stain_number
+        for i in range(self.ica_.shape[2]):
+            # On calcule le cout: Cost(Vmin, P)
+            cost = self.getCost(v_min[:, i])
+            while(d[i] < d_min):
+                #ici jai pas fait le calcule de U, cause de manque d'information
+                V = v_min[:, i] + d[i]
+                k = self.getCost(V)
+                if k < cost :
+                    v_min[:, i] = V
+                    cost = k
+                else:
+                    d[i] = max(d[i]/10, d_min)
+        return v_min
+
+    def getCost(self, Vmin):
+        v_min = np.zeros(self.ica_.shape)
+        list_d = []
+        for i in range(3):
+            list_d.append( np.linalg.norm(self.P[:, i]) + ( np.dot(Vmin, self.P[:,i].T)**2 / np.linalg.norm(Vmin)**2) )
+
+
+
+    def normalisation2(self, mat):
+        mat = 255.0 * (mat - mat.min()) / (mat.max() - mat.min())
+        return mat
 
     # ---------------------------------------------Test---------------------------------------------------#
     def transformData(self, img):                                                                 #-------#
@@ -131,7 +159,16 @@ class ICA_StainSeparation:
 # ---------------------------------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    img = plt.imread("../DataSet/BreastCancerCell_dataset/ytma10_010704_benign1_ccd.tif")
+    #img_path ="../DataSet/BreastCancerCell_dataset/ytma10_010704_benign1_ccd.tif"
+    #img_path = "../DataSet/arn1.tif"
+    #img_path = "../Stain/Lung_4stain.tif"
+    #img_path = "../Stain/Lung_4stain02.tif"
+    #img_path = "../he.png"
+
+    img_path="/home/ro0t34/Documents/projet/images/Nouveau dossier/imageOrig1.bmp"
+
+    img = plt.imread(img_path)
+    img = img[:, :, 0:3]
     l,c, d = img.shape
     #img = cv2.resize(img, (c/3, l/3))
     print img.shape
@@ -139,37 +176,110 @@ if __name__ == "__main__":
     stain.RGB_2_OD()
     stain.lanchePCA()
     stain.lanchICA()
+    stain.correctVectorICA(3)
     # Resahepe et visalisation des resultats
-    img_pca = stain.pca_.reshape((l, c, 3))
-    img_ica = stain.ica_.reshape((l, c, 3))
+    img_pca = stain.normalisation2(stain.pca_).reshape((l, c, 3))
+    img_ica = stain.normalisation2(stain.ica_).reshape((l, c, 3))
 
-    plt.subplot(3, 1, 1)
+    fig = plt.figure(1, figsize=(15, 12))
+    plt.subplots_adjust(left=0.02, right=0.98, bottom=0.02, top=0.98)
+
+    plt.subplot(5, 3, 1)
+    plt.title("PCA 1")
+    plt.imshow(img_pca[:, :, 0], cmap="gray")
+
+    plt.subplot(5, 3, 2)
+    plt.title("PCA 2")
+    plt.imshow(img_pca[:, :, 1], cmap="gray")
+
+    plt.subplot(5, 3, 3)
+    plt.title("PCA 3")
+    plt.imshow(img_pca[:, :, 2], cmap="gray")
+
+    pca1 = np.uint8(img_pca[:, :, 0])
+    pca2 = np.uint8(img_pca[:, :, 1])
+    pca3 = np.uint8(img_pca[:, :, 2])
+
+    thres, pca1 = cv2.threshold(pca1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thres2, pca2 = cv2.threshold(pca2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thres3, pca3 = cv2.threshold(pca3, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    plt.subplot(5, 3, 4)
+    plt.title("PCA 1")
+    plt.imshow(pca1, cmap="gray")
+
+    plt.subplot(5, 3, 5)
+    plt.title("PCA 2")
+    plt.imshow(pca2, cmap="gray")
+
+    plt.subplot(5, 3, 6)
+    plt.title("PCA 3")
+    plt.imshow(pca3, cmap="gray")
+
+    plt.subplot(5, 3, 7)
+    plt.title("ICA 1")
+    plt.imshow(img_ica[:, :, 0], cmap="gray")
+
+    plt.subplot(5, 3, 8)
+    plt.title("ICA 2")
+    plt.imshow(img_ica[:, :, 1], cmap="gray")
+
+    plt.subplot(5, 3, 9)
+    plt.title("ICA 3")
+    plt.imshow(img_ica[:, :, 2], cmap="gray")
+
+    ica1 = np.uint8(img_ica[:, :, 0])
+    ica2 = np.uint8(img_ica[:, :, 1])
+    ica3 = np.uint8(img_ica[:, :, 2])
+
+    thres, ica1 = cv2.threshold(ica1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thres2, ica2 = cv2.threshold(ica2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    thres3, ica3 = cv2.threshold(ica3, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    plt.subplot(5, 3, 12)
+    plt.title("ICA 1")
+    plt.imshow(ica1, cmap="gray")
+
+    plt.subplot(5, 3, 10)
+    plt.title("ICA 2")
+    plt.imshow(ica2, cmap="gray")
+
+    plt.subplot(5, 3, 11)
+    plt.title("ICA 3")
+    plt.imshow(255 - ica3, cmap="gray")
+
+    ica = cv2.merge((ica1, ica3, ica2))
+    pca = cv2.merge((pca3, pca2, pca1))
+
+    plt.subplot(5, 3, 13)
+    plt.title("PCA ")
+    plt.imshow(pca)
+
+    plt.subplot(5, 3, 14)
+    plt.title("ICA ")
+    plt.imshow(ica)
+
+    plt.subplot(5, 3, 15)
     plt.title("Original")
     plt.imshow(img)
 
-    plt.subplot(3, 3, 4)
-    plt.title("ICA R")
-    plt.imshow(img_ica[:, :, 0], cmap="gray")
-
-    plt.subplot(3, 3, 5)
-    plt.title("ICA G")
-    plt.imshow(img_ica[:, :, 1], cmap="gray")
-
-    plt.subplot(3, 3, 6)
-    plt.title("ICA B")
-    plt.imshow(img_ica[:, :, 2], cmap="gray")
-
-    plt.subplot(3, 3, 7)
-    plt.title("PCA R")
-    plt.imshow(img_pca[:, :, 0], cmap="gray")
-
-    plt.subplot(3, 3, 8)
-    plt.title("PCA G")
-    plt.imshow(img_pca[:, :, 1], cmap="gray")
-
-    plt.subplot(3, 3, 9)
-    plt.title("PCA B")
-    plt.imshow(img_pca[:, :, 2], cmap="gray")
-
     plt.show()
 
+    # ce code permet de sauvegarder les images des méthodes, il faut juste donner le chemin
+    import cv2
+    path="/home/ro0t34/Pictures/"
+
+    cv2.imwrite(path + "img.png", img)
+
+    cv2.imwrite(path + "pca1.png", img_pca[:, :, 0])
+    cv2.imwrite(path + "pca2.png", img_pca[:, :, 1])
+    cv2.imwrite(path + "pca3.png", img_pca[:, :, 2])
+
+    cv2.imwrite(path + "ica1.png", img_ica[:, :, 0])
+    cv2.imwrite(path + "ica2.png", img_ica[:, :, 1])
+    cv2.imwrite(path + "ica3.png", img_ica[:, :, 2])
+
+    import Image
+    img1 = Image.fromarray(img_ica[:, :, 0])
+    print img1
+    img1.save(path + "image.tiff")
